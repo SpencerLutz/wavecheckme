0b79f0ce08c142e933e7fd2c8c2990ef1c5f8dfb
SpencerLutz <spencerlutz1@gmail.com>
2022-04-09 17:56:35 -0400

    Backend proof of concept

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -2,12 +2,21 @@ import React from 'react'
 import './Homepage.css';
 import SearchBar from './components/SearchBar';
 
+
 function Homepage() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <h1>WaveCheckme</h1>
         <SearchBar placeholder="Enter a song" />
-      
+      <p>{data}</p>
     </div>
   );
 }
