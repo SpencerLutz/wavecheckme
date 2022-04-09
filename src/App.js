6c4876540da4125835ed878a4836f4daf7d68ac3
SpencerLutz <spencerlutz1@gmail.com>
2022-04-09 11:52:50 -0400

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          i stick my fingers up my ass
         </p>
         <a
           className="App-link"
