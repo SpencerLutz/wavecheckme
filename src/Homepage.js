55a7af3c827899279abba1e31e4ea43c2b29456a
Alejandro-Echaniz <a.echaniz02@gmail.com>
2022-04-09 14:35:39 -0400

    feat: basic textbar functionality

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Homepage.css';
+import SearchBar from './components/SearchBar';
 
 function Homepage() {
   return (
     <div className="App">
+        <SearchBar />
       <h1>WaveCheckme</h1>
     </div>
   );
 }
-
+ 
 export default Homepage;
