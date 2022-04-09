0867735ddd832607e7143b14b7065758b59100a9
SpencerLutz <spencerlutz1@gmail.com>
2022-04-09 14:46:03 -0400

    Rearranged files for server

---
+++ client/src/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Homepage.css';
+import SearchBar from './components/SearchBar';
+
+function Homepage() {
+  return (
+    <div className="App">
+        <SearchBar placeholder="Enter a song" />
+      <h1>WaveCheckme</h1>
+      <h2>    </h2>
+      <h2>enter song...</h2>
+    </div>
+  );
+}
+ 
+export default Homepage;
