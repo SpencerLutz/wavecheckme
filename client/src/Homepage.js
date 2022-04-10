08f08bd30c3d2d8e34fdb6a867fb57964e86f5e1
SpencerLutz <spencerlutz1@gmail.com>
2022-04-10 03:40:46 -0400

    Fixed to match mgus25

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -3,6 +3,7 @@ import './Homepage.css';
 import SearchBar from './components/SearchBar';
 import { useRef, useState } from 'react';
 import Modal from "./components/Modal";
+import styles from "./components/Modal.module.css";
 
 function Homepage() {
   const [isOpen, setIsOpen] = useState(false)
@@ -11,17 +12,13 @@ function Homepage() {
   return (
     <div className="App">
       <img src="favicon_io/wvm.svg" alt=""/>
-      <h1 style={{ color: 'white' }}>WaveCheck Me!</h1>
-      <h1></h1><h1></h1><h1></h1> 
+      <div style={{fontSize: '70px', color: 'white'}}>WaveCheck Me! </div>
       <h3 style={{ color: 'white' }}>Search for a song and we'll find you something similar. </h3>
       <SearchBar placeholder="Enter a song" setSearchQuery={(q) => {
         console.log(q);
         setSearchQuery(q);
         setIsOpen(true);
       }}/>
-      <button onClick={() => setIsOpen(true)}>
-        Open Modal
-      </button>
       {isOpen && <Modal setIsOpen={setIsOpen} searchQuery={searchQuery} />}
       <h1></h1><h1></h1><h1></h1><h1></h1>
       <h5 style={{ color: 'white' }}>BitCamp 2022: Alex Dobrzycki, Alejandro Echaniz, Maia Gustafson, and Spencer Lutz</h5>
