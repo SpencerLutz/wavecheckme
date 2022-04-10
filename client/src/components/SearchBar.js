53eaa3df23f8400d1f3c6d8ecd39f91e7917f82e
Alejandro-Echaniz <a.echaniz02@gmail.com>
2022-04-10 00:19:49 -0400

    feat: functionality that opens modal when hitting enter

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./SearchBar.css";
+import Modal from "./Modal"
 
 function SearchBar({ placeholder, data}) {
+  const [isOpen, setIsOpen]= useState(false);
+
   function submit(query) {
     fetch('/getsong?search='+query)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {console.log(data);
+     setIsOpen(true)}).catch((err) => setIsOpen(true));
 
   }
 
@@ -16,12 +20,13 @@ function SearchBar({ placeholder, data}) {
               if(e.code == "Enter" ) {
                 let term = encodeURIComponent(e.target.value).replace(/%20/g, "+");
                 submit(term);
+                
               }
             }}/>
 
             <div className='"searchIcon"'></div>
         </div>
-        
+        {isOpen && <Modal setIsOpen={setIsOpen} />}
         <div className="dataResult"></div>
     </div>
   )
