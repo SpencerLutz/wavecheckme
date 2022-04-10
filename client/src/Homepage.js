b0200b0d4b9f8805b84e30a5d36197962926235c
mgus25 <103299747+mgus25@users.noreply.github.com>
2022-04-10 03:20:08 -0400

    hi

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -3,6 +3,8 @@ import './Homepage.css';
 import SearchBar from './components/SearchBar';
 import { useRef, useState } from 'react';
 import Modal from "./components/Modal";
+import styles from "./components/Modal.module.css";
+
 
 function Homepage() {
   const [isOpen, setIsOpen] = useState(false)
@@ -10,13 +12,7 @@ function Homepage() {
   return (
     <div className="App">
       <img src="favicon_io/wvm.svg" alt=""/>
-
-      <div className={styles.title}>
-            WaveCheck Me!
-      </div>
-
-      
-      <h1></h1><h1></h1><h1></h1> 
+       <div style={{fontSize: '70px', color: 'white'}}>WaveCheck Me! </div>
       <h3 style={{ color: 'white' }}>Search for a song and we'll find you something similar. </h3>
       <SearchBar placeholder="Enter a song" />
       <button onClick={() => setIsOpen(true)}>
