eb3d88b385482cd9180e859f7ea2a0c4fa0c6ae0
SpencerLutz <spencerlutz1@gmail.com>
2022-04-10 01:02:42 -0400

    Resolved issues

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./SearchBar.css";
+import Modal from "./Modal"
 
 function SearchBar({placeholder}) {
+  const [isOpen, setIsOpen]= useState(false);
 
   async function submit(query) {
     const responses = await fetch('/getsong?search='+query)
   .then(response => response.json());
+    setIsOpen(true);
     return responses;
   }
 
   return (
     <div className="search">
-        <div className="searchInputs">
-            <input type="text" placeholder={placeholder} onKeyDown={(e) => {
-              if(e.code == "Enter") {
-                document.getElementById("test").innerHTML = "HERE";
-                let term = e.target.value;//encodeURIComponent(e.target.value).replace(/%20/g, "+");
-                submit(term).then(data => {
-                  console.log(data.results[0]["previewUrl"]); // JSON data parsed by `data.json()` call
-                  var audio = new Audio(data.results[0]["previewUrl"]);
-                  audio.play();
-                });
-              }
-            }}/>
-
-            <div className='"searchIcon"'></div>
-        </div>
-        
-        <div className="dataResult"></div>
+      <div className="searchInputs">
+        <input type="text" placeholder={placeholder} onKeyDown={(e) => {
+          if(e.code == "Enter") {
+            document.getElementById("test").innerHTML = "HERE";
+            let term = e.target.value;//encodeURIComponent(e.target.value).replace(/%20/g, "+");
+            submit(term).then(data => {
+              console.log(data.results[0]["previewUrl"]); // JSON data parsed by `data.json()` call
+              var audio = new Audio(data.results[0]["previewUrl"]);
+              audio.play();
+            });
+          }
+        }}/>
+      </div>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
     </div>
   )
 }
