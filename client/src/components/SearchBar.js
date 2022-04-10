d2fe5723755f9961629476b096d8bc2c2b75ec7c
SpencerLutz <spencerlutz1@gmail.com>
2022-04-10 00:24:46 -0400

    Added playing functionality

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import "./SearchBar.css";
 
-function SearchBar({ placeholder, data}) {
-  function submit(query) {
-    fetch('/getsong?search='+query)
-  .then(response => response.json())
-  .then(data => console.log(data));
+function SearchBar({placeholder}) {
 
+  async function submit(query) {
+    const responses = await fetch('/getsong?search='+query)
+  .then(response => response.json());
+    return responses;
   }
 
   return (
     <div className="search">
         <div className="searchInputs">
+            <p id="test">No Result Yet</p>
             <input type="text" placeholder={placeholder} onKeyDown={(e) => {
-              if(e.code == "Enter" ) {
-                let term = encodeURIComponent(e.target.value).replace(/%20/g, "+");
-                submit(term);
+              if(e.code == "Enter") {
+                document.getElementById("test").innerHTML = "HERE";
+                let term = e.target.value;//encodeURIComponent(e.target.value).replace(/%20/g, "+");
+                submit(term).then(data => {
+                  console.log(data.results[0]["previewUrl"]); // JSON data parsed by `data.json()` call
+                  var audio = new Audio(data.results[0]["previewUrl"]);
+                  audio.play();
+                });
               }
             }}/>
 
