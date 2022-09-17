60f3a485332d284cea81a3a3da238a3c827f359f
SpencerLutz <spencerlutz1@gmail.com>
2022-09-16 22:58:44 -0400

    Fix indentation with tabs

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -9,24 +9,24 @@ function Homepage() {
 	const [isOpen, setIsOpen] = useState(false)
 	const [searchQuery, setSearchQuery] = useState();
 
-  	return (
-    	<div className="App">
-      		<img src="favicon_io/wvm.svg" alt=""/>
-      		<div style={{fontSize: '70px', color: 'white'}}>WaveCheck Me! </div>
-      		<h3 style={{ color: 'white' }}>Search for a song and we'll find you something similar. </h3>
-      
+	return (
+		<div className="App">
+			<img src="favicon_io/wvm.svg" alt=""/>
+			<div style={{fontSize: '70px', color: 'white'}}>WaveCheck Me! </div>
+			<h3 style={{ color: 'white' }}>Search for a song and we'll find you something similar. </h3>
+	  
 			{/* this was the code written by the smart dude, need to look more into it to understand what's going on*/}
 			<SearchBar placeholder="Enter a song" setSearchQuery={(q) => {
 				setSearchQuery(q);
 				setIsOpen(true);
 			}}/>
-    
-	  		{isOpen && <Modal setIsOpen={setIsOpen} searchQuery={searchQuery} />}
-    
-	  		<h1></h1><h1></h1><h1></h1><h1></h1> 
-      		<h5 style={{ color: 'white' }}>BitCamp 2022: Alex Dobrzycki, Alejandro Echaniz, Maia Gustafson, and Spencer Lutz</h5>
-    	</div>
-  	);
+	
+			{isOpen && <Modal setIsOpen={setIsOpen} searchQuery={searchQuery} />}
+	
+			<h1></h1><h1></h1><h1></h1><h1></h1> 
+			<h5 style={{ color: 'white' }}>BitCamp 2022: Alex Dobrzycki, Alejandro Echaniz, Maia Gustafson, and Spencer Lutz</h5>
+		</div>
+	);
 }
  
 export default Homepage;
